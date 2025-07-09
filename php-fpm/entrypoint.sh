bc88b2c3540da3a2cae9f9c38e440fca27e01544
bperraud <benjamin.perraudin@ulb.be>
2025-07-09 17:09:11 +0200

    fix entrypoint

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -2,18 +2,18 @@
 set -e
 
 # Start php-fpm in the background
-/usr/sbin/php-fpm* -c /etc/php/cur/fpm/php-fpm.conf &
+/usr/sbin/php-fpm8.2 -c /etc/php/cur/fpm/php-fpm.conf &
 
 # Optional: wait for it to start up
 sleep 5  # Or add health probe here
 
 # Run setup script
-# echo "[INFO] Running setup script..."
-# ./var/www/html/scripts/setup.sh
+echo "[INFO] Running setup script..."
+/var/www/html/scripts/setup.sh
 
 # # Run post-install script
-# echo "[INFO] Running post script..."
-# ./var/www/html/scripts/post_install.sh
+echo "[INFO] Running post script..."
+/var/www/html/scripts/post_install.sh
 
 # Run cron job manually
 echo "[INFO] Running cronjob..."
