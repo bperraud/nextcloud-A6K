42091da972be3d7698f096979486602a6864dc2e
bperraud <benjamin.perraudin@ulb.be>
2025-07-09 15:28:40 +0200

    entrypoint to run setup and post script

---
+++ php-fpm/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Start php-fpm in the background
+php-fpm -c /etc/php/cur/fpm/php-fpm.conf &
+
+# Optional: wait for it to start up
+sleep 5  # Or add health probe here
+
+# Run setup script
+echo "[INFO] Running setup script..."
+./var/www/html/scripts/setup.sh
+
+# Run post-install script
+echo "[INFO] Running post script..."
+./var/www/html/scripts/post_install.sh
+
+# Run cron job manually
+echo "[INFO] Running cronjob..."
+php /var/www/html/cron.php
+
+# Wait for php-fpm to keep container alive
+wait %1
+
