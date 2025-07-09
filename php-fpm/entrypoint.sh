cc86928fcb33d08d6941b7472882c372b6e1b7f4
bperraud <benjamin.perraudin@ulb.be>
2025-07-09 16:22:45 +0200

    back to default php cmd

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -2,18 +2,19 @@
 set -e
 
 # Start php-fpm in the background
-php-fpm -c /etc/php/cur/fpm/php-fpm.conf &
+/usr/bin/php-fpm -c /etc/php/cur/fpm/php-fpm.conf &
+# CMD ["/usr/bin/php-fpm", "-c", "/etc/php/cur/fpm/php-fpm.conf"]
 
 # Optional: wait for it to start up
 sleep 5  # Or add health probe here
 
 # Run setup script
-echo "[INFO] Running setup script..."
-./var/www/html/scripts/setup.sh
+# echo "[INFO] Running setup script..."
+# ./var/www/html/scripts/setup.sh
 
-# Run post-install script
-echo "[INFO] Running post script..."
-./var/www/html/scripts/post_install.sh
+# # Run post-install script
+# echo "[INFO] Running post script..."
+# ./var/www/html/scripts/post_install.sh
 
 # Run cron job manually
 echo "[INFO] Running cronjob..."
