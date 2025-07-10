fd9d04c6ef9d130d05bfe22ce1b5c55e37afb4f6
bperraud <benjamin.perraudin@ulb.be>
2025-07-10 14:20:54 +0200

    scripts directory separate from nextcloud install

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -9,11 +9,11 @@ sleep 5  # Or add health probe here
 
 # Run setup script
 echo "[INFO] Running setup script..."
-/var/www/html/scripts/setup.sh
+/scripts/setup.sh
 
 # # Run post-install script
 echo "[INFO] Running post script..."
-/var/www/html/scripts/post_install.sh
+/scripts/post_install.sh
 
 # Run cron job manually
 echo "[INFO] Running cronjob..."
