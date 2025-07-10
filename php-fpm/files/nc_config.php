5cfe215432cad2ef716e917d1754b45bde1cf654
bperraud <benjamin.perraudin@ulb.be>
2025-07-10 15:59:01 +0200

    clamav config

--- php-fpm/files/nc_config.php
+++ php-fpm/files/nc_config.php
@@ -2,7 +2,7 @@
 
 $CONFIG = [
   'trusted_proxies' => ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16'],
-  'overwrite.cli.url' => 'https://apache:8443',
+  'overwrite.cli.url' => 'https://localhost',
   'trusted_domains' => [
     'localhost',
     'nextcloud',
